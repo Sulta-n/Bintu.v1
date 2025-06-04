a7f129fa456691d57ce5683d1e2fe960e7c3c5c1
Aleem <meliodaslord280@gmail.com>
2025-06-04 15:35:38 +0100

    fixed cards issue

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,30 +12,26 @@ const cards = [
 
 const About = () => {
   return (
-    <section id="about" className="relative py-24 p-4">
+    <section id="about" className="relative py-24 px-4">
       <p className="text-light text-sm">Get to Know </p>
       <h1 className="text-primary text-2xl md:text-3xl mb-8 font-bold">
         About Me
       </h1>
       <div className="container mx-auto max-w-5xl">
-        <div className="grid grid-cols-1 md:grid-cols-[35%_50%] gap-[15%] md:items-center max-md:grid-cols-1 max-sm:gap-[12%]">
+        <div className="grid grid-cols-1 lg:grid-cols-[35%_50%] lg:gap-[15%] gap-[12%]">
           {/* image section */}
-          <div className="w-full max-md:w-[50%] mx-auto bg-gradient-to-r from-transparent via-primary to-transparent grid place-items-center rounded-[2rem]">
+          <div className="w-md max-md:w-[50%] mx-auto bg-gradient-to-r from-transparent via-primary to-transparent grid place-items-center rounded-[2rem]">
             <div className="aspect-square rotate-[10deg] rounded-[2rem] hover:rotate-0 transition-transform duration-300">
-              <img src={aboutpic} alt="" className="rounded-[2rem]" />
+              <img src={aboutpic} alt="" className="rounded-[2rem] w-md" />
             </div>
           </div>
 
-          {/* cards-section */}
-          <div className="w-full h-full">
-            <div
-              className="grid grid-cols-1 
-              sm:grid-cols-2
-            md:grid-cols-3 place-items-center md:gap-x-24 max-sm:gap-y-7 max-sm:gap-x-8"
-            >
+          {/* Cards */}
+          <div className="w-full h-full lg:ml-5">
+            <div className="grid grid-cols-2 md:grid-cols-3 gap-[1rem]">
               {cards.map((card) => (
-                <article className="flex flex-col items-center justify-center space-y-3 card max-w-fit ">
-                  <h1 className="text-primary text-[1.7rem]">{card.icon}</h1>
+                <article className="card flex flex-col justify-between items-center ">
+                  <h2 className="text-primary text-[1.7rem]">{card.icon}</h2>
                   <h5 className="text-white  text-[0.9rem] font-bold">
                     {card.text}
                   </h5>
@@ -45,22 +41,21 @@ const About = () => {
                 </article>
               ))}
             </div>
+            <p className="text-light my-4 text-sm text-left">
+              A passionate full-stack developer with a keen eye for building
+              seamless digital experiences. With expertise in both front-end and
+              back-end technologies, I craft scalable solutions that blend
+              functionality with design. Whether it's developing dynamic web
+              applications, optimizing databases, or architecting APIs, I thrive
+              on solving complex challenges with clean and efficient code.
+            </p>
+            <a
+              href="#contact"
+              className="button flex justify-center items-center lg:justify-start"
+            >
+              Let's Talk
+            </a>
           </div>
-          {/* <div className=" w-full h-full">
-            <div className="grid grid-cols-3  place-items-center">
-              {cards.map((card) => (
-                <article className="card items-center place-items-center  text-white cursor-default w-[150px] h-[150px]">
-                  <h1 className="text-primary text-[1.4rem] mb-3">
-                    {card.icon}
-                  </h1>
-                  <h5 className="text-white text-[0.9rem]">{card.text}</h5>
-                  <small className="text-light text-[0.7rem] whitespace-nowrap">
-                    {card.xp}
-                  </small>
-                </article>
-              ))}
-            </div>
-          </div> */}
         </div>
       </div>
     </section>
